45b6c81416ff088f8829197e7bcafe2e38fd6a84
Alfian Yusuf Abdullah <alfian@dicoding.com>
2026-01-03 15:48:36 +0700

    refactor(routes): update download endpoint route to download-file

--- app-downloader/app/routes.ts
+++ app-downloader/app/routes.ts
@@ -3,5 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("api/downloads/stream", "routes/api.downloads.stream.ts"),
-    route("api/download/:id", "routes/api.download.$id.ts"),
+    route("api/download/:id", "routes/download-file.ts"),
 ] satisfies RouteConfig;
